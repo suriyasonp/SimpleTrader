1584b4481984bbdb85f2d8645bb60d8408b2aae0
Sonphu Suriya <suriya.sonphu@gmail.com>
2021-06-16 17:26:38 +0700

    Calling Finance API (Not finish)

---
+++ SimpleTrader.Domain/Models/MajorIndex.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTrader.Domain.Models
+{
+    public enum MajorIndexType
+    {
+        DowJoines,
+        Nasdaq,
+        SP500
+    }
+    public class MajorIndex
+    {
+        public double Price { get; set; }
+        public double Changes { get; set; }
+        public MajorIndexType Type { get; set; }
+    }
+}
---
+++ SimpleTrader.FinancialModelingPrepAPI/Services/MajorIndexService.cs
@@ -0,0 +1,22 @@
+﻿using SimpleTrader.Domain.Models;
+using SimpleTrader.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTrader.FinancialModelingPrepAPI.Services
+{
+    public class MajorIndexService : IMajorIndexService
+    {
+        public Task<MajorIndex> GetMajorIndex(MajorIndexType indexType)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                await client.GetAsync("https://financialmodelingprep.com/api/v3/profile/AAPL?apikey=demo");
+            }
+        }
+    }
+}
