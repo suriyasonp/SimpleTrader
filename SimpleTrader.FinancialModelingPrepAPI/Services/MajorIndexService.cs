35d88dfa30e890e2c8eda56ff07a38806f15e3b4
Sonphu Suriya <suriya.sonphu@gmail.com>
2021-06-17 17:25:33 +0700

    commit

--- SimpleTrader.FinancialModelingPrepAPI/Services/MajorIndexService.cs
+++ SimpleTrader.FinancialModelingPrepAPI/Services/MajorIndexService.cs
@@ -1,11 +1,7 @@
 ﻿using Newtonsoft.Json;
 using SimpleTrader.Domain.Models;
 using SimpleTrader.Domain.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace SimpleTrader.FinancialModelingPrepAPI.Services
--- SimpleTrader.WPF/State/Navigators/Navigator.cs
+++ SimpleTrader.WPF/State/Navigators/Navigator.cs
@@ -1,12 +1,6 @@
 ﻿using SimpleTrader.WPF.Commands;
 using SimpleTrader.WPF.Models;
 using SimpleTrader.WPF.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace SimpleTrader.WPF.State.Navigators
